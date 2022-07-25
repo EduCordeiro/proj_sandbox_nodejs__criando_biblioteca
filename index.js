a45dd63eda3bfad590e4a9c7e412578765bbabae
Eduardo Cordeiro Maia Monteiro <edufacfito@gmail.com>
2022-07-25 20:33:33 -0300

    Retornando links como array de objetos atravez de exec da regex

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
-
-const texto = 'A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo. São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)';
-
 function extraiLinks(texto){
     const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
-    const linkExtraidos = texto.match(regex);
-    console.log(linkExtraidos);
-}
+    const arrayResultados = [];
 
-extraiLinks(texto);
+    let temp;
+    while( (temp = regex.exec(texto) ) !== null){
+      arrayResultados.push({ [temp[1]] : temp[2] });      
+    }
+
+    return  arrayResultados;
+}
 
 function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'Caminho ou arquivo não encontrado o arquivo.'));
@@ -21,7 +22,7 @@ async function pegaArquivo(caminhoDoArquivo){
 
     try{
       const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-      console.log(chalk.green(texto));
+      console.log(extraiLinks(texto));
     }catch(erro){
       trataErro(erro);
     };    
