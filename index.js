30fba5e29f3a15fed58eb1be4a38c283390d6072
Eduardo Cordeiro Maia Monteiro <edufacfito@gmail.com>
2022-07-25 19:15:56 -0300

    usando asybc e await

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'Caminho ou arquivo não encontrado o arquivo.'));
 }
 
-
-function pegaArquivo(caminhoDoArquivo){
+async function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
 
-    fs.promises
-    .readFile(caminhoDoArquivo, encoding)
-    .then((texto) => console.log(chalk.green(texto)))
-    .catch((erro)=> trataErro(erro))
+    try{
+      const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+      console.log(chalk.green(texto));
+    }catch(erro){
+      trataErro(erro);
+    };    
 }
 
 pegaArquivo('./arquivos/texto1.md');
