56a5e0eaeccdb25cb8c2743df14a72e78e898dd9
Eduardo Cordeiro Maia Monteiro <edufacfito@gmail.com>
2022-07-26 15:58:53 -0300

    Implementando testes com Jest | describe

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,17 @@
 const pegaArquivo = require('../index');
 
-test('deve ser uma funcao', () => {
-  expect(typeof pegaArquivo).toBe('function');
+const arrayResult = [
+    {
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+    }
+];
+
+describe('Função pegaArquivo', () => {
+    it("deve ser uma função", () => {
+        expect(typeof pegaArquivo).toBe('function');
+    })
+    it('deve retornar array com resultados', async () => {
+        const resultado = await pegaArquivo('D:/Workspaces/NodeJs/proj_sandbox_nodejs__criando_biblioteca/test/arquivos/texto1.md');
+        expect(resultado).toEqual(arrayResult);
+    })
 });
