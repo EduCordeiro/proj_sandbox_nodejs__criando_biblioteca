df134370f808c3c5bf7cd8b71ed4f4d2d1b1ac7e
Eduardo Cordeiro Maia Monteiro <edufacfito@gmail.com>
2022-07-26 12:34:33 -0300

    Montando o objeto, parte 2

--- http-validacao.js
+++ http-validacao.js
@@ -1,26 +1,45 @@
 //const fetch = require("node-fetch");
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
+function manejaErros(erro){
+  throw new Error(erro.message);
+}
+
 async function checaStatus(arrayURLs){
   // Promises assync await
-  const arrayStatuss = await Promise.all(arrayURLs.map(async url => {
-    const res = await fetch(url);
-    return res.status;
-  }));
+  try{
+    const arrayStatuss = await Promise
+                              .all(arrayURLs
+                              .map(async url => {
+                                    const res = await fetch(url);
+                                    return res.status;
+    }));
 
-  return arrayStatuss;
+    return arrayStatuss;
+  }catch(erro){
+    manejaErros(erro);
+  }
 
 }
 
 function geraArrrayDeURLs(arrayLinks){
-  return arrayLinks.map(objetoLink => Object.values(objetoLink).join());  
+  return arrayLinks.map(objetoLink => Object
+                                      .values(objetoLink)
+                                      .join());  
 }
 
 async function validaURLs(arrayLinks){
   const links = geraArrrayDeURLs(arrayLinks);
   const statuslink = await checaStatus(links);
+  console.log(arrayLinks, statuslink)
 
-return statuslink;
+  //spread operator
+  const resultados = arrayLinks.map((objeto, indice) => ({
+    ...objeto,
+    status : statuslink[indice]
+  }));
+
+  return resultados;
 
 }
 
