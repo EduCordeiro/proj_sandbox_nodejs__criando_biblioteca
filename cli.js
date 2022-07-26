74b08fb28b6dd743ec7122e4fa523a79798a9b12
Eduardo Cordeiro Maia Monteiro <edufacfito@gmail.com>
2022-07-26 11:44:06 -0300

    Adicionando opções

--- cli.js
+++ cli.js
@@ -1,11 +1,17 @@
 const chalk = require('chalk');
 const pegaArquivo = require('./index');
+const validaURLs = require('./http-validacao');
+
 const caminho = process.argv;
 
 async function processaTexto(caminhoDeArquivo){
     const resultado = await pegaArquivo(caminhoDeArquivo[2]);
-    console.log('Lista de linkls', resultado);
-}
 
+    if(caminho[3] === 'validar'){
+        console.log('links validados', validaURLs(resultado));
+    }else{
+        console.log('Lista de linkls', resultado);
+    }
+}
 
 processaTexto(caminho);
